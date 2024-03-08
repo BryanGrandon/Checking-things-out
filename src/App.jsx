0ff41bfbdaad5f643468928fa51d31a47c845c72
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-03-08 11:12:40 -0300

    feat(web):add Post components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Post } from "./components/Post/Post";
 import { Users } from "./components/Users/Users";
 
 function App() {
   return (
     <>
       <h1>JSON Placeholder</h1>
-      <Users />
+      <article>
+        <h2>Users</h2>
+        <Users />
+      </article>
+      <article>
+        <h2>Posts</h2>
+        <Post />
+      </article>
     </>
   );
 }
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,17 @@
+import "./Post.css";
+
+export function Post() {
+  return (
+    <section className="post">
+      <h3 className="post__title">
+        sunt aut facere repellat provident occaecati excepturi optio
+        reprehenderit
+      </h3>
+      <p className="post__body">
+        quia et suscipit\nsuscipit recusandae consequuntur expedita et
+        cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem
+        sunt rem eveniet architecto
+      </p>
+    </section>
+  );
+}
