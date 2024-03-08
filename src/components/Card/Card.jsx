584038a85317584735d997c99f49cc536fb7ae0c
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-03-07 21:51:25 -0300

    feat(web):add reset-css-styles, fetchData.js and component Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhone,
+  faEnvelope,
+  faGlobe,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+import "./Card.css";
+
+export function UserCard({ name, email, phone, website, company }) {
+  return (
+    <>
+      <article className="card">
+        <section className="card__header">
+          <p className="card__company">{company}</p>
+        </section>
+        <section className="card__info">
+          <p className="card__name">
+            <span className="icon">
+              <FontAwesomeIcon icon={faUser} />
+            </span>{" "}
+            {name}
+          </p>
+          <p className="card__email">
+            <span className="icon">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </span>
+            {email}
+          </p>
+          <p className="card__phone">
+            <span className="icon">
+              <FontAwesomeIcon icon={faPhone} />
+            </span>
+            {phone}
+          </p>
+          <p className="card__website">
+            <span className="icon">
+              <FontAwesomeIcon icon={faGlobe} />
+            </span>
+            <a href={website}>{website}</a>
+          </p>
+        </section>
+      </article>
+    </>
+  );
+}
