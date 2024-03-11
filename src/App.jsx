843a79b8a806fb23f21e56335ceb22a2342848f3
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-03-11 17:12:18 -0300

    styles(web):add App.css and change other styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Posts } from "./components/Posts/Posts";
 import { Users } from "./components/Users/Users";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>JSON Placeholder</h1>
-      <article>
-        <h2>Users</h2>
+      <h1 className="title">JSON Placeholder</h1>
+      <article className="article">
+        <h2 className="title__h2">Users</h2>
         <Users />
       </article>
-      <article>
-        <h2>Posts</h2>
+      <article className="article">
+        <h2 className="title__h2">Posts</h2>
         <Posts />
       </article>
     </>
