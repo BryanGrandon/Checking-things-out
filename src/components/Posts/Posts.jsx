5ceb335ee075f9da5c2d50a004e693fda4c113af
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-03-12 13:01:04 -0300

    feat(web):change Pots.jsx to add conditional rendering

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { fetchData } from "../../service/fetchData";
 import { Post } from "../Post/Post";
 import "./Posts.css";
@@ -7,13 +7,30 @@ const postData = fetchData("https://jsonplaceholder.typicode.com/posts");
 
 export function Posts() {
   const data = postData.read();
+
+  const [render, setRender] = useState(5);
+  const updateRender = () => setRender(render + 5);
+
   return (
     <Suspense>
-      <section className="posts">
-        {data.map((post) => (
-          <Post key={post.id} title={post.title} text={post.body} />
-        ))}
-      </section>
+      <>
+        <section className="posts">
+          {data.map((post) => {
+            if (post.id <= render) {
+              return <Post key={post.id} title={post.title} text={post.body} />;
+            }
+          })}
+        </section>
+        {data.at(-1).id === render ? (
+          ""
+        ) : (
+          <section className="post__section_btn">
+            <button className="post__btn" onClick={updateRender}>
+              More Posts
+            </button>
+          </section>
+        )}
+      </>
     </Suspense>
   );
 }
