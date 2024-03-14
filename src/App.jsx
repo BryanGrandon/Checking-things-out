c8c38d2e6b113f1e8567717b81276ba2bb54c1ee
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-03-14 15:34:21 -0300

    feat(web):add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import { Posts } from "./components/Posts/Posts";
 import { Users } from "./components/Users/Users";
-import "./App.css";
 import { Photos } from "./components/Photos/Photos";
+import { Navbar } from "./components/Navbar/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 className="title">JSON Placeholder</h1>
-      <article className="article">
-        <h2 className="title__h2">Users</h2>
-        <Users />
-      </article>
-      <article className="article">
-        <h2 className="title__h2">Posts</h2>
-        <Posts />
-      </article>
-      <article className="article">
-        <h2 className="title__h2">Photos</h2>
-        <Photos />
-      </article>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <article className="article" id="users">
+          <h2 className="title__h2">Users</h2>
+          <Users />
+        </article>
+        <article className="article" id="posts">
+          <h2 className="title__h2">Posts</h2>
+          <Posts />
+        </article>
+        <article className="article" id="photos">
+          <h2 className="title__h2">Photos</h2>
+          <Photos />
+        </article>
+      </main>
     </>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faFile, faImage } from "@fortawesome/free-solid-svg-icons";
+import "./Navbar.css";
+
+export function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <a href="#" className="logo">
+          JSON Placeholder
+        </a>
+        <ul className="navbar__list">
+          <li>
+            <a href="#users" className="navbar__element">
+              Users
+            </a>
+          </li>
+          <li>
+            <a href="#posts" className="navbar__element">
+              Posts
+            </a>
+          </li>
+          <li>
+            <a href="#photos" className="navbar__element">
+              Photos
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <nav className="lower-navigation">
+        <a href="#users" className="lower-navigation__element">
+          <FontAwesomeIcon icon={faUser} className="lower-navigation__icon" />
+          <p className="lower-navigation__text">Users</p>
+        </a>
+
+        <a href="#posts" className="lower-navigation__element">
+          <FontAwesomeIcon icon={faFile} className="lower-navigation__icon" />
+          <p className="lower-navigation__text">Posts</p>
+        </a>
+
+        <a href="#photos" className="lower-navigation__element">
+          <FontAwesomeIcon icon={faImage} className="lower-navigation__icon" />
+          <p className="lower-navigation__text">Photos</p>
+        </a>
+      </nav>
+    </>
+  );
+}
