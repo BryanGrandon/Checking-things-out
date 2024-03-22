c945796f0be9fbcc292e6939530da3bdda494746
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-03-22 14:02:58 -0300

    refactor(web):change App.jsx and pages/projects.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Projects from "./pages/projects/projects";
 import NotFound from "./pages/not-found/not-found";
 import Contact from "./pages/contact/contact";
 import "./App.css";
+import ProjectsUsers from "./containers/projects/projects-users/projects-users";
+import ProjectsTodos from "./containers/projects/projects-todos/projects-todos";
+import ProjectsPosts from "./containers/projects/projects-posts/projects-posts";
+import ProjectsPhotos from "./containers/projects/projects-photos/projects-photos";
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/users" element={<ProjectsUsers />} />
+          <Route path="/projects/todos" element={<ProjectsTodos />} />
+          <Route path="/projects/posts" element={<ProjectsPosts />} />
+          <Route path="/projects/photos" element={<ProjectsPhotos />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -1,18 +1,7 @@
-import UsersArticle from "../../components/article/users-article/users-article";
-import PostsArticle from "../../components/article/posts-article/posts-article";
-import PhotosArticle from "../../components/article/photo-article/photo-article";
-import TodosArticle from "../../components/article/todos-article/todos-article";
-
 export default function Projects() {
   return (
     <>
-      <UsersArticle />
-
-      <PostsArticle />
-
-      <PhotosArticle />
-
-      <TodosArticle />
+      <h2>Projects</h2>
     </>
   );
 }
