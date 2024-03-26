cf68cd230468836a1687d9bb64ef3fafd4d14459
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-03-25 21:47:55 -0300

    feat(web):add new Hero component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import Navbar from "../../components/navbar/navbar";
+import Hero from "../../components/hero/hero";
+import homeSVG from "../../assets/home.svg";
 import "./home.css";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <header className="header">
-        <h1 className="title">Project React</h1>
-        <p className="header__text">
-          Un lugar agradable para conocer personas mediante blogs o albunes de
-          fotografia
-        </p>
-      </header>
+
+      <Hero
+        title="JSON Project"
+        paragraph="JSON Project offers you a space to create a blog to your liking. Meet people who have the same tastes as you."
+        svg={homeSVG}
+      />
 
       <article className="project-container">
         <article className="content">
           <h2 className="content__title"></h2>
-          <p></p>
         </article>
       </article>
     </>
