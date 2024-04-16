85225697bac1a3ebd6b09a5092280305dcff53f4
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-04-16 17:47:20 -0400

    (web):remove files

--- src/other/hero-image/hero-image.jsx
+++
@@ -1,9 +0,0 @@
-import "./hero-image-styles.css";
-
-export default function HeroImage({ svg, alt }) {
-  return (
-    <article className="hero-image">
-      <img className="hero-image-svg" src={svg} alt={alt} />
-    </article>
-  );
-}
