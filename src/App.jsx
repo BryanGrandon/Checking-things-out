aa88e35f5b0eed1645ebc241adc7c6439f8f12ca
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-01-22 14:07:20 -0300

    feat(web):add dropzone for extractColors

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import { extractColors } from 'extract-colors'
 import './app.css'
-import Values from 'values.js'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 function App () {
   const [imageColor, setImageColor] = useState([])
   const [srcImg, setSrcImg] = useState('')
-  const [theColor, setTheColor] = useState('')
-  const [TColor, setTColor] = useState([])
-
-  useEffect(() => {
-    setTheColor('#ff0000')
-    setTColor(new Values('#ff0000').all(20))
-  }, [])
 
   const palette = async (img) => {
     const output = await extractColors(img)
@@ -28,40 +20,61 @@ function App () {
     reader.readAsDataURL(e.target.files[0])
   }
 
-  const handlerClickT = (color) => {
-    setTheColor(color)
-    setTColor(new Values(color).all(20))
+  const handlerClickE = async (ev) => {
+    ev.preventDefault()
+
+    let url = ''
+    const element = ev.dataTransfer.items
+
+    if (element) {
+      [...element].forEach((item, i) => {
+        if (item.kind === 'file') {
+          const file = item.getAsFile()
+          url = file
+        }
+      })
+    } else {
+      [...ev.dataTransfer.files].forEach((file, i) => {
+        url = file
+      })
+    }
+
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      setSrcImg(e.target.result)
+      palette(e.target.result)
+    }
+    reader.readAsDataURL(url)
+    console.log(url)
+  }
+
+  function dragOverHandler (ev) {
+    console.log('File(s) in drop zone')
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault()
   }
 
   return (
     <main>
       <section>
-        <input type='file' name='file' id='img-file' onChange={(e) => handlerClick(e)} />
+        <section className='dropzone' onDrop={(ev) => handlerClickE(ev)} onDragOver={(ev) => dragOverHandler(ev)}>
+          <p>Drag & Drop </p>
+          <p>Icon</p>
+        </section>
+
+        <input type='file' name='file' onChange={(e) => handlerClick(e)} />
 
         <img src={`${srcImg}`} alt='K' width='20%' />
-
-        <section>
-          {imageColor.map((e) => (
-            <p key={Math.random() * 100} style={{ backgroundColor: `${e.hex}` }} className='colors'>
-              {e.hex}
-            </p>
-          ))}
-        </section>
       </section>
 
-      <section className='color-sect'>
-        {TColor.map((e) => (
-          <p
-            key={Math.random() * 10}
-            style={{ background: `#${e.hex}` }}
-            onClick={() => handlerClickT(`#${e.hex}`)}
-            className={theColor === `#${e.hex}` ? 'card active' : 'card'}
-          >
+      <section>
+        {imageColor.map((e) => (
+          <p key={Math.random() * 100} style={{ backgroundColor: `${e.hex}` }} className='colors'>
             {e.hex}
           </p>
         ))}
       </section>
-      <section />
+
     </main>
   )
 }
