e4470f11581a0e8c0da6c4e02c66518559c7eba5
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-04-17 18:48:38 -0400

    testing tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,26 @@
-import { extractColors } from 'extract-colors'
 import './app.css'
-import { useState } from 'react'
 
-function App () {
-  const [imageColor, setImageColor] = useState([])
-  const [srcImg, setSrcImg] = useState('')
-
-  const palette = async (img) => {
-    const output = await extractColors(img)
-    setImageColor(output)
-  }
-
-  const handlerClick = async (e) => {
-    const reader = new FileReader()
-    reader.onload = (e) => {
-      setSrcImg(e.target.result)
-      palette(e.target.result)
-    }
-    reader.readAsDataURL(e.target.files[0])
-  }
-
-  const handlerClickE = async (ev) => {
-    ev.preventDefault()
-
-    let url = ''
-    const element = ev.dataTransfer.items
-
-    if (element) {
-      [...element].forEach((item, i) => {
-        if (item.kind === 'file') {
-          const file = item.getAsFile()
-          url = file
-        }
-      })
-    } else {
-      [...ev.dataTransfer.files].forEach((file, i) => {
-        url = file
-      })
-    }
-
-    const reader = new FileReader()
-    reader.onload = (e) => {
-      setSrcImg(e.target.result)
-      palette(e.target.result)
-    }
-    reader.readAsDataURL(url)
-    console.log(url)
-  }
-
-  function dragOverHandler (ev) {
-    console.log('File(s) in drop zone')
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault()
-  }
-
+const App = () => {
   return (
-    <main>
-      <section>
-        <section className='dropzone' onDrop={(ev) => handlerClickE(ev)} onDragOver={(ev) => dragOverHandler(ev)}>
-          <p>Drag & Drop </p>
-          <p>Icon</p>
+    <main className='bg-gray-200 h-screen p-4 flex flex-col gap-4'>
+      <h1 className='text-4xl text-blue-600 font-bold'>Welcome to React</h1>
+      <div className='flex items-center justify-center border border-black border-solid rounded-lg h-30 bg-red-600 shadow-xl'>
+        <p>Box</p>
+      </div>
+
+      <article>
+        <h2 className='text-3xl font-semibold text-purple-800'>Title 2</h2>
+        <section className='grid grid-cols-[repeat(auto-fill,_minmax(20rem,_1fr))] border border-blue-600 border-solid rounded-2xl overflow-hidden shadow'>
+          <div className='flex items-center justify-center bg-yellow-400 text-2xl'>1</div>
+          <div className='flex items-center justify-center bg-yellow-400 text-2xl'>2</div>
+          <div className='flex items-center justify-center bg-yellow-400 text-2xl'>3</div>
+          <div className='flex items-center justify-center bg-yellow-400 text-2xl'>4</div>
+          <div className='flex items-center justify-center bg-yellow-400 text-2xl'>5</div>
+          <div className='flex items-center justify-center bg-yellow-400 text-2xl'>6</div>
         </section>
-
-        <input type='file' name='file' onChange={(e) => handlerClick(e)} />
-
-        <img src={`${srcImg}`} alt='K' width='20%' />
-      </section>
-
-      <section>
-        {imageColor.map((e) => (
-          <p key={Math.random() * 100} style={{ backgroundColor: `${e.hex}` }} className='colors'>
-            {e.hex}
-          </p>
-        ))}
-      </section>
-
+      </article>
     </main>
   )
 }
+
 export default App
